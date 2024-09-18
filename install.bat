471ad9387cd46b2ab9bac2041a1c4df882bf6b3f
neomasterr <neomasterr@mail.ru>
2024-09-18 22:07:07 +0400

    master initial commit

---
+++ install.bat
@@ -0,0 +1,43 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+set version=0.2.3rc3
+set tag=%version%-2
+set name=goodbyedpi-%tag%
+set archive=%name%.zip
+
+pushd %localappdata%
+del %archive%
+curl -L https://github.com/ValdikSS/GoodbyeDPI/releases/download/%version%/goodbyedpi-%tag%.zip -o %archive%
+powershell Expand-Archive %archive% -DestinationPath %name%
+cd %name%
+start service_install_russia_blacklist_YOUTUBE.cmd
+popd
